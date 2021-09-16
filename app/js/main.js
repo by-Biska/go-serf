243985adfba6c362860f26b8d38477930061b823
By-Biska <varakinkirill3@gmail.com>
2021-09-16 21:37:24 +0300

    sleep

--- app/js/main.js
+++ app/js/main.js
@@ -31,18 +31,26 @@ $(function () {
       '<img class="slider-arrows slider-arrows__left" src="images/arrow-left.svg" alt="">',
     nextArrow:
       '<img class="slider-arrows slider-arrows__right" src="images/arrow-right.svg" alt="">',
-      asNavFor: ".slider-map",
+    asNavFor: ".slider-map",
   });
 
-
-  $(".slider-map").slick ({
+  $(".slider-map").slick({
     slidesToShow: 8,
     slidesToScroll: 3,
     infinite: false,
-    arrows:false,
-    asNavFor: '.surf-slider',
-    focusOnSelect: true
-  })
+    arrows: false,
+    asNavFor: ".surf-slider",
+    focusOnSelect: true,
+  });
+
+  $(".holder__slider").slick({
+    infinite: true,
+    fade: true,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src="images/arrow-left.svg" alt="">',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src="images/arrow-right.svg" alt="">',
+  });
 
   // Date
   var d = new Date();
@@ -58,4 +66,52 @@ $(function () {
   }
   $(".date__year").text(d.getFullYear());
 
+  // PLUS / MINUS
+  $(
+    '<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="images/plus.svg"></div><div class="quantity-button quantity-down"><img src="images/minus.svg"></div></div>'
+  ).insertAfter(".quantity input");
+  $(".quantity").each(function () {
+    var spinner = $(this),
+      input = spinner.find('input[type="number"]'),
+      btnUp = spinner.find(".quantity-up"),
+      btnDown = spinner.find(".quantity-down"),
+      min = input.attr("min"),
+      max = input.attr("max");
+
+    btnUp.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue >= max) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue + 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+    btnDown.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+  });
+
+  $(".quantity-button").on("click", function () {
+    let summ =
+      $(".nights").val() * $(".summ").data("nights") +
+      ($(".guests").val() - 1) * $(".summ").data("guests");
+
+    $(".summ").html(summ);
+  });
+
+  let summ =
+    $(".nights").val() * $(".summ").data("nights") +
+    ($(".guests").val() - 1) * $(".summ").data("guests");
+
+  $(".summ").html(summ);
 });
